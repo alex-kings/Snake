9905e9fb049d1db9af0b40ef77c65b1e836b65a5
alex-kings <alexandrekings1@gmail.com>
2022-04-20 12:57:25 +0100

    Organised game, snake and body classes.

--- Snake.js
+++ Snake.js
@@ -1,29 +1,86 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext('2d');
-const tileNb = 10;
-const tileSize = canvas.width/tileNb;
+let xpos = 0;
+let ypos = 0;
+
 
 export function startGame(){
     game();
 }
 
-let xpos = 0;
-let ypos = 0;
 
-function game(){
-    empty();
-    drawBody(xpos, 0);
-    xpos++;
-    window.setTimeout(game, 300);
+
+
+class Game{
+    canvas = document.getElementById("canvas");
+    ctx = this.canvas.getContext('2d');
+    tileNb = 10;
+    tileSize = this.canvas.width/this.tileNb;
+    gameSpeed = 5;
+
+    bgColour = "yellow";
+
+
+    headImg = new Image(0,0);
+    bodyImg = new Image();
+    tailImg = new Image();
+
+    snake = new Snake(5,5);
+
+    constructor(){
+        this.headImg.src = "./Images/snake_head.jpg";
+        this.bodyImg.src = "./Images/snake_body.jpg";
+        this.tailImg.src = "../Images/snake_tail.jpg";
+    }
+
+    game(){
+        empty();
+        drawHead(0,0, 180);
+        window.setTimeout(game, 1000/gameSpeed);
+    }
+    
+    empty(){
+        ctx.fillStyle = bgColour;
+        ctx.fillRect(0,0,canvas.width, canvas.height);
+    }
+    
+    drawHead(x, y, deg){
+        ctx.save();
+        ctx.translate((x+0.5)*tileSize,(y+0.5)*tileSize);
+        ctx.rotate(deg*Math.PI/180);
+        ctx.drawImage(headImage, -0.5*tileSize,-0.5*tileSize,tileSize, tileSize);
+        ctx.restore();
+    }
+
+
+    // Draws the snake on canvas according to its data.
+    drawSnake(){
+        
+    }
 }
 
-function empty(){
-    ctx.fillStyle = 'yellow';
-    ctx.fillRect(0,0,canvas.width, canvas.height);
+class Snake{
+    headX = 0;
+    headY = 0;
+    tailX = 1;
+    tailY = 1;
+    length = 0; // The length of the body following the head.
+    bodyParts = [];
+    headDirection = 0;
+
+    constructor(x, y){
+        this.headX = x;
+        this.headY = y;
+    }
+
+
 }
 
-function drawBody(xpos, ypos){
-    ctx.fillStyle='green';
-    ctx.fillRect(xpos*tileSize, ypos*tileSize, tileSize, tileSize);
+class Body{
+    x = 0;
+    y = 0;
+    color="green";
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+    }
 }
 
