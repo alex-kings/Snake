97f9eff3cb59b1f62f46acba3c4d9c3184461f04
alex-kings <alexandrekings1@gmail.com>
2023-04-22 12:29:06 +0100

    Push changes.

--- Game.js
+++ Game.js
@@ -6,9 +6,9 @@ class Game{
     constructor(){
         this.canvas = document.getElementById("canvas");
         this.ctx = this.canvas.getContext('2d');
-        this.tileNb = 10;
+        this.tileNb = 15;
         this.tileSize = this.canvas.width/this.tileNb;
-        this.gameSpeed = 1;
+        this.gameSpeed = 5;
         this.bgColour = "yellow";
         this.headImg = new Image(0,0);
         this.bodyImg = new Image();
@@ -18,13 +18,21 @@ class Game{
         this.tailImg.src = "./Images/snake_tail.png";
 
         this.snake = new Snake(5,5);
+        this.fruit = new Fruit(2,2);
+        this.gameEnd = false;
     }
 
     // Function called at each game tick
     gameTick(){
         this.empty();
+        this.moveSnake();
+        this.fruitCollision();
         this.drawSnake();
-        this.snake.move();
+        this.drawFruit();
+        if(this.snake.bodyCollision()) {
+            this.gameEnd = true;
+            console.log("ENNNND")
+        }
     }
 
     empty(){
@@ -36,7 +44,20 @@ class Game{
      * Draws the snake at the correct location.
      */
     drawSnake(){
+        this.drawBody();
         this.drawHead();
+    }
+
+    moveSnake(){
+        this.snake.prevHeadX = this.snake.headX;
+        this.snake.prevHeadY = this.snake.headY;
+        this.snake.headX += this.snake.xDir;
+        this.snake.headY += this.snake.yDir;
+        if(this.snake.headX < 0) this.snake.headX = this.tileNb-1;
+        if(this.snake.headY < 0) this.snake.headY = this.tileNb-1;
+        if(this.snake.headX === this.tileNb) this.snake.headX = 0;
+        if(this.snake.headY === this.tileNb) this.snake.headY = 0;
+        this.snake.moveBody();
     }
     
     /**
@@ -49,6 +70,52 @@ class Game{
         this.ctx.drawImage(this.headImg, -0.5*this.tileSize,-0.5*this.tileSize,this.tileSize,this.tileSize);
         this.ctx.restore();
     }
+    
+    /**
+     * Draws the body of the snake.
+     */
+    drawBody(){
+        this.snake.bodyParts.forEach(bodyPart => {
+            this.ctx.fillStyle = bodyPart.colour;
+            this.ctx.fillRect(bodyPart.x * this.tileSize, bodyPart.y * this.tileSize, this.tileSize, this.tileSize);
+        })
+    }
+
+    /**
+     * Places a fruit on a random position on the board.
+     */
+    placeNewFruit(){
+        this.fruit.x = Math.floor(Math.random()*this.tileNb);
+        this.fruit.y = Math.floor(Math.random()*this.tileNb);
+    }
+
+    /**
+     * Draws the fruit on the board.
+     */
+    drawFruit() {
+        this.ctx.fillStyle = this.fruit.colour;
+        this.ctx.fillRect(this.fruit.x*this.tileSize, this.fruit.y*this.tileSize, this.tileSize, this.tileSize);
+    }
+
+    /**
+     * Checks if the snake head collisions with the fruit.
+     */
+    fruitCollision(){
+        if(this.snake.headX === this.fruit.x && this.snake.headY === this.fruit.y){
+            //Fruit eaten.
+            this.placeNewFruit();
+            this.gameSpeed++;
+            this.snake.addBodyPart(this.snake.headX, this.snake.headY);
+        }
+    }
+
+    /**
+     * Ends the game.
+     */
+    end(){
+        this.ctx.fillStyle = "black";
+        this.ctx.fillRect(0,0,this.canvas.width, this.canvas.height);
+    }
 }
 
 /**
@@ -57,7 +124,9 @@ class Game{
 class Snake{
     headX = 0;
     headY = 0;
-    headDeg = 0;
+    prevHeadX = 0;
+    prevHeadY = 0;
+    headDeg = 270;
     tailX = 1;
     tailY = 1;
     length = 0; // The length of the body following the head.
@@ -72,20 +141,58 @@ class Snake{
     }
 
     /**
-     * Updates the position of the snake.
-     */
-    move(){
-        this.headX += this.xDir;
-        this.headY += this.yDir;
-    }
-    /**
      * Changes the direction of the snake.
      * @param {int} xDir The new x direction.
      * @param {int} xDir The new y direction.
      */
     changeDirection(xDir, yDir){
-        this.xDir = xDir;
-        this.yDir = yDir;
+        if(this.xDir + xDir != 0) this.xDir = xDir; 
+        if(this.yDir + yDir != 0) this.yDir = yDir;
+
+        this.headOrientation();
+    }
+
+    /**
+     * Orients the head.
+     */
+    headOrientation(){
+        if(this.xDir === 1) this.headDeg = 270;
+        if(this.xDir === -1) this.headDeg = 90;
+        if(this.yDir === 1) this.headDeg = 0;
+        if(this.yDir === -1) this.headDeg = 180;
+    }
+
+    /**
+     * Adds a bodypart at position x-y.
+     * @param {int} x The x position of the bodypart
+     * @param {int} y The y position of the bodypart
+     */
+    addBodyPart(x, y){
+        this.bodyParts.unshift(new Body(x,y));
+    }
+    
+    /**
+     * Moves the body of the snake.
+     */
+    moveBody(){
+        // Moves the body if there is no bodyPart at the head position.
+        if(this.bodyParts.length === 0) return null;
+        if(this.bodyParts[0].x === this.headX && this.bodyParts[0].y === this.headY){
+            return null
+        }
+        this.bodyParts.pop();
+        this.addBodyPart(this.prevHeadX, this.prevHeadY);
+    }
+
+    /**
+     * Determines whether the head enters in collision with a bodypart.
+     */
+    bodyCollision(){
+        if(this.bodyParts.length < 2) return false;
+        for(let i = 1; i < this.bodyParts.length; i++){
+            if(this.bodyParts[i].x === this.headX && this.bodyParts[i].y === this.headY) return true
+        }
+        return false;
     }
 }
 
@@ -95,10 +202,21 @@ class Snake{
 class Body{
     x = 0;
     y = 0;
-    color="green";
+    colour="green";
     constructor(x,y){
         this.x = x;
         this.y = y;
+    }
+}
+
+/**
+ * Class for fruits.
+ */
+class Fruit{
+    constructor(x,y){
+        this.x = x;
+        this.y = y;
+        this.colour = "red";
     }
 }
 
@@ -112,7 +230,6 @@ const game = new Game();
 window.addEventListener("keydown",(event)=>{
     switch (event.code) {
         case "ArrowUp":
-            console.log("up")
             game.snake.changeDirection(0,-1);
             break;
         case "ArrowDown":
@@ -131,8 +248,11 @@ window.addEventListener("keydown",(event)=>{
 })
 
 function gameLoop(){
-    game.gameTick();
-    window.setTimeout(gameLoop, 1000/game.gameSpeed);
+    if(!game.gameEnd){
+        game.gameTick();
+        window.setTimeout(gameLoop, 1000/game.gameSpeed);
+    }
+    else game.end();
 }
 
 export function startGame(){
